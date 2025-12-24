72b915cb145ee2050a9f94e3b14c23aed243a860
Julian <spamhiervoll@hotmail.com>
2025-12-24 15:06:01 +0100

    test2

--- .github/actions/extract_version/extract_version.py
+++ .github/actions/extract_version/extract_version.py
@@ -2,16 +2,15 @@ import xml.etree.ElementTree as ET
 import os
 import sys
 
+PACKAGE_XML = os.path.join(os.getcwd(), "package.xml")  # repo root
+
 try:
-    tree = ET.parse("package.xml")
+    tree = ET.parse(PACKAGE_XML)
     root = tree.getroot()
     version_elem = root.find("version")
-
     if version_elem is None:
         raise ValueError("No <version> tag found")
-
     version = version_elem.text.strip()
-
 except Exception as e:
     print(f"Error extracting version: {e}", file=sys.stderr)
     sys.exit(1)
